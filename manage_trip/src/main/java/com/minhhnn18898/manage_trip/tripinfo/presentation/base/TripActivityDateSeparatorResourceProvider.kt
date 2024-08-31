8f221948c2c5296db316e2a90df44197d6347a85
Nhat Minh <huynhnguyennhatminh98@gmail.com>
2024-08-31 11:43:30 +0700

    [Trip Detail] Change the order of day separator

--- manage_trip/src/main/java/com/minhhnn18898/manage_trip/tripinfo/presentation/base/TripActivityDateSeparatorResourceProvider.kt
+++ manage_trip/src/main/java/com/minhhnn18898/manage_trip/tripinfo/presentation/base/TripActivityDateSeparatorResourceProvider.kt
@@ -8,10 +8,10 @@ class TripActivityDateSeparatorResourceProvider  @Inject constructor() {
 
     private val dateSeparatorResource = listOf(
         R.drawable.trip_activity_date_separator_leaf,
-        R.drawable.trip_activity_date_separator_tree,
-        R.drawable.trip_activity_date_separator_forest,
         R.drawable.trip_activity_date_separator_autumn_tree_house,
+        R.drawable.trip_activity_date_separator_tree,
         R.drawable.trip_activity_date_separator_autumn_road,
+        R.drawable.trip_activity_date_separator_forest,
         R.drawable.trip_activity_date_separator_autumn_city
     )
 
